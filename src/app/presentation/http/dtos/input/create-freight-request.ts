1b2c17ed409eeeb605a5e2b5e6f9707865a85e51
Adrian L. Medeiros <lima.adrian13@gmail.com>
2025-07-04 08:26:36 -0300

    refactor: update dto create-freight-request.ts

--- src/app/presentation/http/dtos/input/create-freight-request.ts
+++ src/app/presentation/http/dtos/input/create-freight-request.ts
@@ -1,11 +1,21 @@
 import { z } from "zod"
 
 export const freightSchema = z.object({
-    description: z.string().min(5).max(30),
-    weight: z.number().min(1).max(4),
-    value: z.number().min(1).max(4),
-    clientId: z.string().length(21),
-    cityId: z.string().length(21)
+  description: z.string().min(5).max(30),
+  weight: z.number()
+    .min(0.01)
+    .max(9999.99)
+    .refine(val => Number.isInteger(val * 100), {
+      message: 'Weight deve ter no máximo duas casas decimais',
+    }),
+  value: z.number()
+    .min(0.01)
+    .max(9999.99)
+    .refine(val => Number.isInteger(val * 100), {
+      message: 'Value deve ter no máximo duas casas decimais',
+    }),
+  clientId: z.string().length(21),
+  cityId: z.string().length(21),
 })
 
 export type CreateFreightRequest = z.infer<typeof freightSchema>
