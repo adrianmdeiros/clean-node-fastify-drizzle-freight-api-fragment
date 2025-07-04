be8d4d536835c0b85906f5581814421c734f0b16
Adrian L. Medeiros <lima.adrian13@gmail.com>
2025-07-04 08:28:16 -0300

    refactor: update dto create-city-request.ts

--- src/app/presentation/http/dtos/input/create-city-request.ts
+++ src/app/presentation/http/dtos/input/create-city-request.ts
@@ -3,7 +3,12 @@ import { z } from "zod"
 export const citySchema = z.object({
     name: z.string().min(10).max(30),
     uf: z.string().min(2).max(2),
-    tax: z.number().min(1).max(4)
+    tax: z.number()
+      .min(0.01)
+      .max(100)
+      .refine(val => Number.isInteger(val * 100), {
+        message: 'Taxa deve ter no máximo duas casas decimais',
+      })
 })
 
 export type CreateCityRequest = z.infer<typeof citySchema>
